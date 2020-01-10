aa0ecbb04adb54613d21d738e9daabb6b2434512
WahyuPrayogo1 <wahyudev3211@gmail.com>
2020-01-10 10:15:09 +0700

    latihan RUMUS MTK

---
+++ Sprint 1/Topik 2/latihan4.php
@@ -0,0 +1,19 @@
+<?php
+$angka = 2;
+$rumus = 3.14;
+$r     = 4.5;
+echo "Default = ",$angka;
+echo "<br>";
+echo "Rumus = ",$rumus;
+echo "<br>";
+echo "Diameter = ", $r;
+echo "<br>";
+$test1 = $angka*$rumus;
+$hasil = $test1*$r;
+
+echo "penyelesain ", $angka, " x ", $rumus, " x ", $r;
+echo "<br>";
+echo "Hasil ", $hasil;
+
+
+ 
